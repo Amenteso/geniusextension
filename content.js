b9085bcefda72b500483e90d4af7f69e4e89148e
Amentes <77643832+Amenteso@users.noreply.github.com>
2025-05-17 19:26:41 +0300

    Update content.js

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Genius Şarkı Sözü Editörü Geliştirme
+// @name         Genius Helper
 // @namespace    http://tampermonkey.net/
 // @version      1.0
 // @description  Genius Helper
